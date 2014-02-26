65f74d54e60336f1aeb1b3b736e97ae9c9626b62
Peter Peterson <petersonpf@ornl.gov>
2014-02-26 09:42:55 -0500

    Re #8943. Removing old form of workspace registration in python.

--- Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/IMaskWorkspace.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/IMaskWorkspace.cpp
@@ -1,7 +1,4 @@
 #include "MantidAPI/IMaskWorkspace.h"
-#include "MantidPythonInterface/kernel/SharedPtrToPythonMacro.h"
-#include "MantidPythonInterface/kernel/Registry/RegisterSingleValueHandler.h"
-
 #include <boost/python/class.hpp>
 
 using Mantid::API::IMaskWorkspace;
@@ -11,12 +8,8 @@ using namespace boost::python;
 /** Python exports of the Mantid::API::IMaskWorkspace class. */
 void export_IMaskWorkspace()
 {
-  REGISTER_SHARED_PTR_TO_PYTHON(IMaskWorkspace);
-
   class_<IMaskWorkspace, boost::noncopyable>("IMaskWorkspace", no_init)
       .def("getNumberMasked", &IMaskWorkspace::getNumberMasked, args("self"),
            "Total number of masked pixels")
       ;
-
-  REGISTER_SINGLEVALUE_HANDLER(IMaskWorkspace_sptr);
 }
