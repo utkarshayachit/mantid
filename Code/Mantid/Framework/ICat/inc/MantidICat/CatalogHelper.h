ad4586c7f21c0afbb13691dc7772603ec8e52ddd
Jay Rainey <jawrainey@gmail.com>
2014-04-01 14:54:13 +0100

    Added templated throwErrorMessage to helper. Refs #9186.

--- Code/Mantid/Framework/ICat/inc/MantidICat/CatalogHelper.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogHelper.h
@@ -1,12 +1,42 @@
 #ifndef MANTID_ICAT_CATALOGHELPER_H_
 #define MANTID_ICAT_CATALOGHELPER_H_
 
+#include <iostream>
+#include <stdexcept>
+
 namespace Mantid
 {
   namespace ICat
   {
     class CatalogHelper
     {
+      public:
+
+        /**
+         * Parse the message returned by ICAT to obtain a user friendly error message.
+         * @param icatProxy :: The PortBindingProxy of the ICAT instance.
+         */
+        template <class T>
+        void throwErrorMessage(T& icatProxy)
+        {
+          char buf[600];
+          icatProxy.soap_sprint_fault(buf,600);
+          std::string error(buf);
+
+          std::string begmsg("<message>");
+          std::string endmsg("</message>");
+
+          auto start = error.find(begmsg);
+          auto end   = error.find(endmsg);
+          std::string exception;
+
+          if(start != std::string::npos && end != std::string::npos)
+          {
+            exception = error.substr(start + begmsg.length(), end - (start + begmsg.length()));
+          }
+
+          throw std::runtime_error(exception);
+        }
 
     };
   }
