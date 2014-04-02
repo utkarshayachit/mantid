ff5c4e593feaa9ebc6a1e41842ff03b32e6f580d
Jay Rainey <jawrainey@gmail.com>
2014-04-02 16:39:37 +0100

    Added better documentation. Refs #9186.

--- Code/Mantid/Framework/ICat/inc/MantidICat/CatalogHelper.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogHelper.h
@@ -9,23 +9,44 @@ namespace Mantid
 {
   namespace ICat
   {
+    /**
+     CatalogHelper is responsible for ensuring consistencies across ICAT catalogs while reducing duplication.
+     For example, the save* methods allow column consistencies across catalogs, which is required
+     for multiple catalogs to work correctly on the GUI.
+
+     @author Jay Rainey, ISIS Rutherford Appleton Laboratory
+     @date 02/04/2014
+     Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+     This file is part of Mantid.
+
+     Mantid is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 3 of the License, or
+     (at your option) any later version.
+
+     Mantid is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     File change history is stored at: <https://github.com/mantidproject/mantid>.
+     Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
     class CatalogHelper
     {
       public:
-
         CatalogHelper();
-
         // Helper method that formats a given timestamp.
         std::string formatDateTime(const time_t &timestamp, const std::string &format);
-
-        // Stores the session details for a specific catalog.
-        API::CatalogSession_sptr session;
-
-      private:
         // Convert a file size to human readable file format.
         std::string bytesToString(int64_t &fileSize);
 
-      public:
+        // Stores the session details for a specific catalog.
+        API::CatalogSession_sptr session;
 
         /**
          * Casts the container's content types from A to B.
