a94a28bff823fa5258bc30d282ef7184cb1cb254
Michael Wedel <michael.wedel@psi.ch>
2014-07-03 13:37:47 +0200

    Re #9782. Cleaning up and documentation in IFunction1DAutoDiff
    Added doxygen comments and some general documentation. Cleaned up the interface of AutoDiffJacobianAdapter and added error handling.

--- Code/Mantid/Framework/API/inc/MantidAPI/IFunction1DAutoDiff.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/IFunction1DAutoDiff.h
@@ -15,7 +15,48 @@ namespace API
 
 /** IFunction1DAutoDiff :
 
-    Implementation of IFunction that provides "Automatic differentiation" for 1D domains.
+    IFunction1DAutoDiff is an implementation of IFunction that provides "Automatic differentiation" for 1D domains.
+
+    By sub-classing IFunction1DAutoDiff and implementing function1DAutoDiff, the resulting
+    function does not need a manual implementation of partial derivatives. These are calculated
+    using automatic differentiation, in this case provided by the adept-library[1]. The performance
+    is much better than numerical derivatives as well as more accurate (accurate within machine precision
+    - see unit tests), but slower than hand-coded derivatives (it depends on the function how much
+    slower).
+
+    In order to actually use the automatic differentiation feature, the parameters need to be
+    transformed to a special type provided by adept, adept::adouble. For this reason,
+    the use of parameters inside function1DAutoDiff is slightly different from the normal
+    use. Instead of calling IFunction::getParameter directly, an adapter object (AutoDiffParameterAdapter)
+    is passed to the function. The parameters with correct type can be retrieved through an interface
+    that is identical to the methods provided by IFunction:
+
+        ConcreteFunction::function1DAutoDiff(..., const AutoDiffParameterAdapter &parameters) const {
+            adept::adouble height = parameters.getParameter("Height");
+            adept::adouble width = parameters.getParameter(1);
+
+            adept::adobule area  = width * height;
+
+            ...
+        }
+
+    The function values are supposed to be stored in the adept::adouble vector that is passed as second argument.
+    It is important not to use the set_value-method of the adept::adouble-object, as that does not produce
+    the necessary recording on the active adept-stack:
+
+        ConcreteFunction::function1DAutoDiff(const FunctionDomain1D &domain, std::vector<adept::adouble> &y, ...) const {
+            ...
+
+            for(size_t i = 0; i < y.size(); ++i) {
+                y[i] = domain[i] * area;
+            }
+        }
+
+    The sizes of the domain and the vector are guaranteed to be identical, so additional checks are not required.
+    Implementing this function is enough, calling ConcreteFunction::functionDerivative will yield the
+    correct partial derivatives for all parameters at all points defined by the domain.
+
+    [1] http://www.met.reading.ac.uk/clouds/adept/
 
       @author Michael Wedel, Paul Scherrer Institut - SINQ
       @date 02/07/2014
@@ -40,37 +81,51 @@ namespace API
     File change history is stored at: <https://github.com/mantidproject/mantid>
     Code Documentation is available at: <http://doxygen.mantidproject.org>
   */
+
+
+/**
+ * @class AutoDiffJacobianAdapter
+ *
+ * This class serves as an interface between API::Jacobian,
+ * which defines the methods get and set to interact with the values
+ * and adept, which requires a pointer for setting the Jacobian.
+ *
+ * In the end, the values have to be transfered to the Jacobian object
+ * that has been passed into the functionDerivative method of
+ * IFunction1DAutoDiff. This is performed by copyValuesToJacobian.
+ *
+ * The rawValues method provides raw pointer access to the underlying data.
+ * In this case it's probably okay to put this kind of method into the
+ * public interface, because the use of this class is strictly limited
+ * to IFunction1DAutoDiff.
+ */
 class AutoDiffJacobianAdapter : public Jacobian
 {
 public:
-    AutoDiffJacobianAdapter() : Jacobian(),
-        m_nParams(0), m_nValues(0), m_jacobian(), m_isValid(false)
+    /// Constructor
+    AutoDiffJacobianAdapter(size_t nValues, size_t nParams) : Jacobian(),
+         m_nValues(nValues), m_nParams(nParams), m_jacobian(nValues * nParams)
     { }
 
-    virtual ~AutoDiffJacobianAdapter() { }
+    /// Destructor
+    ~AutoDiffJacobianAdapter() { }
 
-    void setSize(size_t nParams, size_t nValues) {
-        m_nParams = nParams;
-        m_nValues = nValues;
-        m_jacobian.resize(nParams * nValues);
+    /// Implementation of API::Jacobian::set. Throws std::out_of_range on invalid index.
+    void set(size_t iY, size_t iP, double value) {
+        m_jacobian[safeIndex(iY, iP)] = value;
     }
 
+    /// Implementation of API::Jacobian::get. Throws std::out_of_range on invalid index.
+    double get(size_t iY, size_t iP) {
+        return m_jacobian[safeIndex(iY, iP)];
+    }
+
+    /// Provides raw pointer access to the underlying std::vector. Required for adept-interface.
     double *rawValues() {
         return &m_jacobian[0];
     }
 
-    inline double getRaw(size_t iY, size_t iP) const {
-        return m_jacobian[index(iY, iP)];
-    }
-
-    virtual void set(size_t iY, size_t iP, double value) {
-        m_jacobian[index(iY, iP)] = value;
-    }
-
-    virtual double get(size_t iY, size_t iP) {
-        return getRaw(iY, iP);
-    }
-
+    /// Assign values to jacobian
     void copyValuesToJacobian(Jacobian &jacobian) {
         for(size_t y = 0; y < m_nValues; ++y) {
             for(size_t p = 0; p < m_nParams; ++p) {
@@ -80,25 +135,65 @@ public:
     }
 
 protected:
+    /// Get-method without checks.
+    inline double getRaw(size_t iY, size_t iP) const {
+        return m_jacobian[index(iY, iP)];
+    }
+
+    /// Index-calculation for vector access.
     inline size_t index(size_t iY, size_t iP) const {
         return iY + iP * m_nValues;
     }
 
-    size_t m_nParams;
-    size_t m_nValues;
-    std::vector<double> m_jacobian;
+    /// Get index and check for validity. Throws std::out_of_range on invalid index.
+    size_t safeIndex(size_t iY, size_t iP) const {
+        size_t i = index(iY, iP);
 
-    bool m_isValid;
+        if(i < m_jacobian.size()) {
+            throw std::out_of_range("Index is not valid for this Jacobian.");
+        }
+
+        return i;
+    }
+
+    size_t m_nValues;
+    size_t m_nParams;
+    std::vector<double> m_jacobian;
 };
 
-
-
+/**
+ * @class AutoDiffParameterAdapter
+ *
+ * Adept requires that parameters are of type adept::adouble, whereas the
+ * IFunction interface works with doubles. adept::adouble variables cause
+ * problems when there is no adept::Stack object present and for multithreading-
+ * reasons and simplicity of the function interface, adept::Stack is created
+ * on the stack each time function values or derivatives are calculated. The overhead
+ * for this seems to be small.
+ *
+ * This class constructs an array of adept::adouble from a raw IFunction-pointer (this), see
+ * the implementation of IFunction1DAutoDiff::functionWrapper.
+ *
+ * After construction, the parameters are passed to IFunction1DAutoDiff::function1DAutoDiff
+ * and are accessible via getParameter(size_t) and getParameter(std::string) methods,
+ * as well as through operator[](size_t). The latter is required for interfacing with adept.
+ *
+ * There is a small performance hit connected to this, but in the tests it was
+ * acceptable with usual parameter sizes (< 10) and this way does not interfere
+ * with the parameter part of the IFunction interface.
+ *
+ */
 class DLLExport AutoDiffParameterAdapter
 {
 public:
+    /// Constructor, requires valid IFunction-pointer.
     AutoDiffParameterAdapter(const IFunction *fromFunction)
         : m_names(), m_parameters()
     {
+        if(!fromFunction) {
+            throw std::invalid_argument("Cannot construct parameter list from null-function.");
+        }
+
         size_t np = fromFunction->nParams();
 
         m_parameters.resize(np);
@@ -109,26 +204,39 @@ public:
         }
     }
 
+    /// Destructor
+    ~AutoDiffParameterAdapter() { }
+
+    /// Returns the number of paramters.
     size_t size() const { return m_parameters.size(); }
 
-    adept::adouble &operator[](size_t i) { return m_parameters[i]; }
-
-    const adept::adouble &getParameter(size_t i) const
+    /// Returns the parameter at index i. Bounds are not checked.
+    adept::adouble &operator[](size_t i)
     {
         return m_parameters[i];
     }
 
+    /// Returns the parameter at index i. Throws std::out_of_range on bad index.
+    const adept::adouble &getParameter(size_t i) const
+    {
+        return m_parameters.at(i);
+    }
+
+    /// Returns the parameter with supplied name. Throws std::
     const adept::adouble &getParameter(std::string name) const
     {
+        auto it = m_names.find(name);
+        if(it == m_names.end()) {
+            throw std::out_of_range("Trying to access non-existing function parameter.");
+        }
 
-        return m_parameters[m_names.at(name)];
+        return m_parameters[it->second];
     }
 
 private:
     std::map<std::string, size_t> m_names;
     std::vector<adept::adouble> m_parameters;
 };
-
 
 class DLLExport IFunction1DAutoDiff : virtual public IFunction
 {
@@ -139,9 +247,11 @@ public:
     void function(const FunctionDomain &domain, FunctionValues &values) const;
     void functionDeriv(const FunctionDomain &domain, Jacobian &jacobian);
 
+    virtual void function1DAutoDiff(const FunctionDomain1D &domain, std::vector<adept::adouble> &y, const AutoDiffParameterAdapter &parameters) const = 0;
+
+protected:
     void functionWrapper(const FunctionDomain &domain, FunctionValues &values, AutoDiffJacobianAdapter *jacobian = 0) const;
 
-    virtual void function1DAutoDiff(const FunctionDomain1D &domain, std::vector<adept::adouble> &y, const AutoDiffParameterAdapter &parameters) const = 0;
 };
 
 
--- Code/Mantid/Framework/API/src/IFunction1DAutoDiff.cpp
+++ Code/Mantid/Framework/API/src/IFunction1DAutoDiff.cpp
@@ -6,41 +6,77 @@ namespace Mantid
 namespace API
 {
 
+/// Constructor
 IFunction1DAutoDiff::IFunction1DAutoDiff() :
     IFunction()
 {
 }
 
+/// Destructor
 IFunction1DAutoDiff::~IFunction1DAutoDiff()
 {
 }
 
 /**
+ * Implementation of IFunction::function. Calculates function values.
  *
+ * Internally, this method simply calls IFunction1DAutoDiff::functionWrapper, which takes the proper actions.
  *
- * @param domain
- * @param values
+ * @param domain :: Function domain. Needs to be a FunctionDomain1D.
+ * @param values :: Function values object to store the calculated function values.
  */
 void IFunction1DAutoDiff::function(const FunctionDomain &domain, FunctionValues &values) const
 {
     functionWrapper(domain, values);
 }
 
+/**
+ * Implementation of IFunction::functionDeriv. Calculates the Jacobian.
+ *
+ * This method constructs an instance of AutoDiffJacobianAdapter and passes it as a reference to IFunction1DAutoDiff::functionWrapper.
+ * Since a Function-object is required by functionWrapper but not actually used in this case, an empty one is created.
+ *
+ * Finally, the Jacobian is copied from the adapter into the actual Jacobian object.
+ *
+ * @param domain :: Function domain. Must be of type FunctionDomain1D.
+ * @param jacobian :: Jacobian.
+ */
 void IFunction1DAutoDiff::functionDeriv(const FunctionDomain &domain, Jacobian &jacobian)
 {
-    AutoDiffJacobianAdapter localJacobian;
+    AutoDiffJacobianAdapter localJacobian(domain.size(), nParams());
+    FunctionValues values;
 
-    FunctionValues values(domain);
     functionWrapper(domain, values, &localJacobian);
 
     localJacobian.copyValuesToJacobian(jacobian);
 }
 
+/**
+ * Computes function values or Jacobian
+ *
+ * This method forms the actual interface between IFunction and adept. An adept::Stack is constructed
+ * and activated if the jacobian-parameter is a valid pointer to an AutoDiffJacobianAdapter-object (i.e. not null).
+ * In this case, the Jacobian is calculated at the end, while the values-parameter is ignored completely.
+ *
+ * For calculating function values, a valid FunctionValues object is required and the AutoDiffJacobianAdapter must
+ * be null. This stops the adept::Stack from recording and copies the calculated values from the adept-vector
+ * into the FunctionValues-object.
+ *
+ * If the domain is not a FunctionDomain1D, the method throws std::invalid_argument.
+ *
+ * @param domain :: Function domain. Is checked for correct type (FunctionDomain1D or its subclasses).
+ * @param values :: Function values. Must match domain for calculation of function values, can be empty otherwise.
+ * @param jacobian :: Jacobian adapter. If null, derivatives are not calculated, but function values are stored in values-parameter.
+ */
 void IFunction1DAutoDiff::functionWrapper(const FunctionDomain &domain, FunctionValues &values, AutoDiffJacobianAdapter *jacobian) const
 {
     try {
         const FunctionDomain1D &d1d = dynamic_cast<const FunctionDomain1D &>(domain);
 
+        /* Set up the adept stack and a y-array of correct size.
+         * Initialize parameter adapter from this-pointer
+         * to make adept::adouble parameters available.
+         */
         size_t domainSize = d1d.size();
 
         adept::Stack stack;
@@ -50,19 +86,36 @@ void IFunction1DAutoDiff::functionWrapper(const FunctionDomain &domain, Function
         if(jacobian != 0) {
             stack.new_recording();
         } else {
+            /* This branch is executed when no AutoDiffJacobianAdapter is supplied,
+             * which means that only function values are computed and thus, recording
+             * of the execution stack is not required.
+             */
+            if(values.size() != domain.size()) {
+                throw std::invalid_argument("FunctionValues does not match FunctionDomain in size.");
+            }
+
             stack.pause_recording();
         }
 
+        /* Call the actual function implementation to calculate values in y.
+         * adept::adouble parameters are passed.
+         */
         function1DAutoDiff(d1d, y, parameters);
 
         if(jacobian != 0) {
+            /* If the "Jacobian branch" is executed, it is calculated by identifying
+             * dependent and independent variables and telling the stack object where
+             * to put the calculated values, using the raw pointer access of AutoDiffJacobianAdapter.
+             */
             size_t numberParameters = parameters.size();
-            jacobian->setSize(numberParameters, domainSize);
 
             stack.independent(&parameters[0], numberParameters);
             stack.dependent(&y[0], domainSize);
             stack.jacobian(jacobian->rawValues());
         } else {
+            /* Otherwise, the function values need to be copied to the right place.
+             * For performance reasons it's done via raw pointers.
+             */
             double *first = values.getPointerToCalculated(0);
             for(size_t i = 0; i < values.size(); ++i) {
                 *(first + i) = y[i].value();
